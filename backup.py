5c02836a7290392025cb73a2ec0699cdb842926d
Kyle Goetke <goetkek@protonmail.com>
2022-04-20 19:53:54 -0400

    Manually set src drive, fixed dest_path formatting, edited robocopy arguments

--- backup.py
+++ backup.py
@@ -14,19 +14,20 @@ while True:
     drive_list = drive_list.split("\x00")[0:-1] # the last element is ""
     for letter in drive_list:
         if win32file.GetDriveType(letter) == win32file.DRIVE_REMOVABLE:
-            drive = "{0}".format(letter)
+            # drive = "{0}".format(letter)
+            drive = "E:\\"
             print("\n--------\n")
             user_check = input(f"Do you want to copy from {drive} (Y/N)?\n> ".format(letter)).upper()
             if user_check[0] == "Y":
                 src_path = f"{drive}"
                 now = datetime.now()
-                current_time = now.strftime("%B %d %Y %H.%M.%S")
-                dest_path = f"D:\\NEW BACKUP {current_time}"
+                current_time = now.strftime("%B_%d_%Y_%H.%M.%S")
+                dest_path = f"D:\\NEW_BACKUP_{current_time}\\"
                 print(f"\nBackup will be located in {dest_path}")
                 start_time = time.time()
                 #! robocopy starts here
                 try:
-                    robocopy_output = subprocess.call(f"robocopy {src_path} {dest_path} /e /copyall", shell=True)
+                    robocopy_output = subprocess.call(f"robocopy {src_path} {dest_path} /E /V /COPYALL /DCOPY:DAT /R:10 /W:5", shell=True)
                     if robocopy_output < 0:
                         print("# Terminated by signal:", -robocopy_output, file=sys.stderr)
                     else:
@@ -36,3 +37,6 @@ while True:
                 #! robocopy ends here
                 total_time = timedelta(seconds=int(time.time() - start_time))
                 print(f"# Files copied, this backup took {total_time} #")
+
+#! https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
+#! https://www.techrepublic.com/article/how-to-quickly-back-up-just-your-data-in-windows-10-with-robocopys-multi-threaded-feature/
